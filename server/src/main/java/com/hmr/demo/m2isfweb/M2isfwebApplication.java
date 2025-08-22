f9e3231dc2ba2c48a84f62498537a1d98fa40b94
jsshin <tb25271@gmail.com>
2025-08-22 09:59:58 +0900

    [init] server source commit

---
+++ server/src/main/java/com/hmr/demo/m2isfweb/M2isfwebApplication.java
@@ -0,0 +1,13 @@
+package com.hmr.demo.m2isfweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2isfwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2isfwebApplication.class, args);
+	}
+
+}
